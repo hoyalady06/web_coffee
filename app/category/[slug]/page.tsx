6032248d9654129d913b8387dc30392e3c870f04
Sharipbay Nursaya <hoyalady06@gmail.com>
2025-12-05 17:28:40 +0600

    исправили текст на наши @category

--- app/category/[slug]/page.tsx
+++ app/category/[slug]/page.tsx
@@ -6,6 +6,18 @@ import { CategoryTabs } from "@/components/catalog/CategoryTabs";
 import { useState, useEffect } from "react";
 import type { Category } from "@/data/products";
 import { ProductCard } from "@/components/products/ProductCard";
+const categoryNames = {
+  cakes:     "Наши торты",
+  pies:      "Наши пироги",
+  bread:     "Наш хлеб",
+  bakery:    "Наша выпечка",
+  desserts:  "Наши пирожные",
+  cookies:   "Наше печенье",
+  icecream:  "Наше мороженое",
+  combo:     "Наше комбо меню",
+  cafe:      "Наше кафе"
+};
+
 
 export default function CategoryPage() {
   const { slug } = useParams();
@@ -21,7 +33,7 @@ export default function CategoryPage() {
   const filtered = allProducts.filter((p) => p.category === currentCategory);
 
   return (
-    <main className="w-full bg-[#fff9f5] pb-20">
+    <main className="w-full   pb-20">
 
       {/* ==== Tabs (как на главной) ==== */}
       <div className="container mx-auto px-6 mt-8">
@@ -34,8 +46,9 @@ export default function CategoryPage() {
       {/* ==== Заголовок (как на главной) ==== */}
       <div className="container mx-auto px-6 mt-10">
         <h1 className="text-3xl font-bold text-[#4b2e16] mb-10">
-          Категория: {currentCategory}
-        </h1>
+  {categoryNames[currentCategory]}
+</h1>
+
 
 {/* Сетка товаров */}
 <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-10 gap-y-16">
