65b630b25946013a2777e8a9d855335a7b80866d
Sharipbay Nursaya <hoyalady06@gmail.com>
2025-12-07 21:21:41 +0600

    это лейоут профиля и там задали свои настройки стиля

--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -1,13 +1,17 @@
+"use client";
+
 import { ProfileSidebar } from "@/components/ProfileSidebar";
 
 export default function ProfileLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex gap-10 max-w-7xl mx-auto py-10" >
+    <div className="container mx-auto px-6 py-10 flex gap-10">
+      <ProfileSidebar />
 
-
-      <main className="flex-1">
-        {children}
-      </main>
+      <div className="flex-1">
+        <div className="bg-white rounded-2xl border border-[#eadfd7] shadow-sm p-10 min-h-[600px]">
+          {children}
+        </div>
+      </div>
     </div>
   );
 }
